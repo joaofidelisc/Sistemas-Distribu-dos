3b458ea022ea9b8771611862117763b773b0f7e7
joaofidelisc <joao.fidelis@estudante.ufscar.br>
2022-04-26 01:11:04 -0300

    atualizado

--- cosmos_get_started.py
+++ cosmos_get_started.py
@@ -1,59 +1,61 @@
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
 import members
 
-# Initialize the Cosmos client
-endpoint = 'https://sistemas-distribuidos-cosmosdb.documents.azure.com:443/'
-key = 'FLrFsEzm7N4eL8ECFpfxFEz5mcSHG6bFYpT47iEyJWbX9WmSTQAe1mSnZiO1Lm40YsX8pkYRSo0Bou91MbHxIg=='
+if __name__ == '__main__':
+    
+    # Initialize the Cosmos client
+    endpoint = 'https://sistemas-distribuidos-cosmosdb.documents.azure.com:443/'
+    key = 'FLrFsEzm7N4eL8ECFpfxFEz5mcSHG6bFYpT47iEyJWbX9WmSTQAe1mSnZiO1Lm40YsX8pkYRSo0Bou91MbHxIg=='
 
-# <create_cosmos_client>
-client = CosmosClient(endpoint, key)
-# </create_cosmos_client>
+    # <create_cosmos_client>
+    client = CosmosClient(endpoint, key)
+    # </create_cosmos_client>
 
-# Create a database
-# <create_database_if_not_exists>
-database_name = 'SeminarioSistemasDistribuidos-CosmosDB'
-database = client.create_database_if_not_exists(id=database_name)
-# </create_database_if_not_exists>
+    # Create a database
+    # <create_database_if_not_exists>
+    database_name = 'SeminarioSistemasDistribuidos-CosmosDB'
+    database = client.create_database_if_not_exists(id=database_name)
+    # </create_database_if_not_exists>
 
-# Create a container
-# Using a good partition key improves the performance of database operations.
-# <create_container_if_not_exists>
-container_name = 'IntegrantesDoGrupo'
-container = database.create_container_if_not_exists(
-    id=container_name, 
-    partition_key=PartitionKey(path="/lastName"),
-    offer_throughput=400
-)
-# </create_container_if_not_exists>
+    # Create a container
+    # Using a good partition key improves the performance of database operations.
+    # <create_container_if_not_exists>
+    container_name = 'IntegrantesDoGrupo'
+    container = database.create_container_if_not_exists(
+        id=container_name, 
+        partition_key=PartitionKey(path="/id"),
+        offer_throughput=400
+    )
+    # </create_container_if_not_exists>
 
+    # Add items to the container
+    member_items_to_create = [members.get_member_joaofidelis(), members.get_member_rafaeltury(), members.get_member_thiagoaraujo()]
 
-# Add items to the container
-member_items_to_create = [members.get_member_joaofidelis(), members.get_member_rafaeltury(), members.get_member_thiagoaraujo()]
+    # <create_item>
+    for member_item in member_items_to_create:
+        container.create_item(body=member_item)
+    # </create_item>
 
- # <create_item>
-for member_item in member_items_to_create:
-    container.create_item(body=member_item)
-# </create_item>
+    # Read items (key value lookups by partition key and id, aka point reads)
+    # <read_item>
+    for members in member_items_to_create:
+        item_response = container.read_item(item=members['id'], partition_key=members['id'])
+        request_charge = container.client_connection.last_response_headers['x-ms-request-charge']
+        print('Read item with id {0}. Operation consumed {1} request units'.format(item_response['id'], (request_charge)))
+    # </read_item>
 
-# Read items (key value lookups by partition key and id, aka point reads)
-# <read_item>
-for members in member_items_to_create:
-    item_response = container.read_item(item=members['id'], partition_key=members['lastName'])
+    # Query these items using the SQL query syntax. 
+    # Specifying the partition key value in the query allows Cosmos DB to retrieve data only from the relevant partitions, which improves performance
+    # <query_items>
+    query = "SELECT * FROM c WHERE c.lastName IN ('Fidelis Cardozo', 'Tury Minatel', 'Araujo')"
+
+    items = list(container.query_items(
+        query=query,
+        enable_cross_partition_query=True
+    ))
+
     request_charge = container.client_connection.last_response_headers['x-ms-request-charge']
-    print('Read item with id {0}. Operation consumed {1} request units'.format(item_response['id'], (request_charge)))
-# </read_item>
 
-# Query these items using the SQL query syntax. 
-# Specifying the partition key value in the query allows Cosmos DB to retrieve data only from the relevant partitions, which improves performance
-# <query_items>
-query = "SELECT * FROM c WHERE c.lastName IN ('Fidelis Cardozo', 'Tury Minatel', 'Araujo')"
+    print('Query returned {0} items. Operation consumed {1} request units'.format(len(items), request_charge))
+    
 
-items = list(container.query_items(
-    query=query,
-    enable_cross_partition_query=True
-))
-
-request_charge = container.client_connection.last_response_headers['x-ms-request-charge']
-
-print('Query returned {0} items. Operation consumed {1} request units'.format(len(items), request_charge))
-# </query_items>
--- cosmos_read_and_delete_item.py
+++ cosmos_read_and_delete_item.py
@@ -2,36 +2,6 @@
 from pydoc import doc
 from typing import Any
 from azure.cosmos import CosmosClient, PartitionKey
-# import cosmos_get_started
-# from container import list_Containers
-
-
-HOST = 'https://sistemas-distribuidos-cosmosdb.documents.azure.com:443/'
-MASTER_KEY = 'FLrFsEzm7N4eL8ECFpfxFEz5mcSHG6bFYpT47iEyJWbX9WmSTQAe1mSnZiO1Lm40YsX8pkYRSo0Bou91MbHxIg=='
-DATABASE_ID = 'SeminarioSistemasDistribuidos-CosmosDB'
-CONTAINER_ID = 'IntegrantesDoGrupo'
-
-
-# <create_cosmos_client>
-client = CosmosClient(HOST, MASTER_KEY)
-# </create_cosmos_client>
-
-# Create a database
-# <create_database_if_not_exists>
-database_name = 'SeminarioSistemasDistribuidos-CosmosDB'
-database = client.create_database_if_not_exists(id=database_name)
-# </create_database_if_not_exists>
-
-# Create a container
-# Using a good partition key improves the performance of database operations.
-# <create_container_if_not_exists>
-container_name = 'IntegrantesDoGrupo'
-container = database.create_container_if_not_exists(
-    id=container_name, 
-    partition_key=PartitionKey(path="/lastName"),
-    offer_throughput=400
-)
-# </create_container_if_not_exists>
 
 def read_items(container):
     print('\n1.3 - Reading all items in a container\n')
@@ -46,15 +16,21 @@ def read_items(container):
     for doc in item_list:
         print('Item Id: {0}'.format(doc.get('id')))
 
+def returnIDS(container):
+    ids_members = []
+    item_list = list(container.read_all_items())
+    for id in item_list:
+        ids_members.append(id.get('id'))
+    return ids_members
+
 def read_item(container, doc_id):
     print('\n1.2 Reading Item by Id\n')
 
     # Note that Reads require a partition key to be spcified.
     response = container.read_item(item=doc_id, partition_key=doc_id)
-    # print('doc_id:', doc_id)
-    # print('\n\n')
     print('Item read by Id {0}'.format(doc_id))
     print('lastName: {0}'.format(response.get('lastName')))
+    print('collegeInformation: {0}'.format(response.get('collegeInformation')))
     # print('collegeInformation: {0}'.format(response.get('collegeInformation')))
 
 
@@ -65,4 +41,23 @@ def delete_item(container, doc_id):
 
     print('Deleted item\'s Id is {0}'.format(doc_id))
 
-read_items(container)
+
+if __name__ == '__main__':
+    endpoint = 'https://sistemas-distribuidos-cosmosdb.documents.azure.com:443/'
+    key = 'FLrFsEzm7N4eL8ECFpfxFEz5mcSHG6bFYpT47iEyJWbX9WmSTQAe1mSnZiO1Lm40YsX8pkYRSo0Bou91MbHxIg=='
+
+    client = CosmosClient(endpoint, key)
+    database_name = 'SeminarioSistemasDistribuidos-CosmosDB'
+    database = client.create_database_if_not_exists(id=database_name)
+
+    container_name = 'IntegrantesDoGrupo'
+    container = database.create_container_if_not_exists(
+        id=container_name, 
+        partition_key=PartitionKey(path="/id"),
+        offer_throughput=400
+    )
+    ids_members = returnIDS(container)
+    #Deleting items
+    for member in ids_members:
+        delete_item(container, member)
+        
