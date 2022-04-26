ff65c7b723c436af34ea692a1a75c19fd900036e
Thiago Araujo Campos <tcampos@vonbraunlabs.com.br>
2022-04-26 20:22:17 -0300

    Comments the code

--- cosmosDB_CRUD.py
+++ cosmosDB_CRUD.py
@@ -1,22 +1,10 @@
-
 from pydoc import doc
 from typing import Any
 import uuid
 from azure.cosmos import CosmosClient, PartitionKey
 
-def read_items(container):
-    print('\n1.3 - Reading all items in a container\n')
-
-    # NOTE: Use MaxItemCount on Options to control how many items come back per trip to the server
-    #       Important to handle throttles whenever you are doing operations such as this that might
-    #       result in a 429 (throttled request)
-    item_list = list(container.read_all_items(max_item_count=10))
-
-    print('Found {0} items'.format(item_list.__len__()))
-
-    for doc in item_list:
-        print('Item Id: {0}'.format(doc.get('id')))
-
+# Recebe o container do CosmosDB e retorna
+# uma lista com os ids de todos os items
 def returnIDS(container):
     ids_members = []
     item_list = list(container.read_all_items())
@@ -24,6 +12,9 @@ def returnIDS(container):
         ids_members.append(id.get('id'))
     return ids_members
 
+# Função disponibilizada na documentação do CosmosDB
+# realiza a leitura de um determinado item pelo doc_id
+# e imprime as informações do item (Id, lastName e collegeInformation)
 def read_item(container, doc_id):
     print('\n1.2 Reading Item by Id')
     # Note that Reads require a partition key to be spcified.
@@ -32,14 +23,17 @@ def read_item(container, doc_id):
     print('lastName: {0}'.format(response.get('lastName')))
     print('collegeInformation: {0}'.format(response.get('collegeInformation')))
 
+# Função disponibilizada na documentação do CosmosDB
+# realiza a deleção de um determinado item pelo doc_id
+# e imprime o id do item deletado
 def delete_item(container, doc_id):
     print('\n1.7 Deleting Item by Id\n')
-
-    response = container.delete_item(item=doc_id, partition_key=doc_id)
+    container.delete_item(item=doc_id, partition_key=doc_id)
 
     print('Deleted item\'s Id is {0}'.format(doc_id))
 
-
+# Recebe os dados para cadastrar um novo membro
+# e retorna um dict formatado com os dados inseridos
 def get_newmember(nome, sobrenome, anoIngresso, curso, RA):
     newMember = {
     'id': nome + str(uuid.uuid4()),
@@ -52,12 +46,16 @@ def get_newmember(nome, sobrenome, anoIngresso, curso, RA):
     }
     return newMember
 
-
+# Mostra o menu principal da aplicação
+# e mantém o loop para realização das operações de CRUD
 def showMenu(container, option = 1):
+
+    # Opção que encerrará o loop
     SAIR = 5
 
     while(option != SAIR):
 
+        # Opções e recebimento da opção selecionada
         print("\n1 - Listar itens do banco de dados")
         print("2 - Deletar item do banco de dados com ID")
         print("3 - Inserir item no banco de dados")
@@ -65,25 +63,35 @@ def showMenu(container, option = 1):
         print(SAIR, "- Sair")
         option = int(input("\nEscolha uma opção: "))
 
+        # Atualiza a lista de membros assim que o usuário
+        # escolhe a opção desejada
         members_ids = returnIDS(container)
 
+        # Lista todos os itens
         if (option == 1):
             if (members_ids):
                 for member in members_ids:
+                    # read_item é a função do azure.cosmos que permite
+                    # a leitura de um item
                     read_item(container, member)
             else:
                 print('Banco de dados vazio!')
 
+        # Deleta um item
         elif (option == 2):
             if (members_ids):
                 for idx, member in enumerate(members_ids):
                     print(idx+1, '-', member)
 
                 delMember = int(input("Escolha uma opção: "))
+
+                # delete_item é a função do azure.cosmos que permite
+                # a remoção de um item
                 delete_item(container, members_ids[delMember-1])
             else:
                 print('Banco de dados vazio!')
 
+        # Cadastra um novo item
         elif (option == 3):
             firstName = str(input("Insira o primeiro nome: "))
             lastName = str(input("Insira o sobrenome: "))
@@ -91,8 +99,12 @@ def showMenu(container, option = 1):
             course = str(input("Insira o curso: "))
             studentId = int(input("Insira o RA: "))
             body = get_newmember(firstName, lastName, yearOfEntry, course, studentId)
+            
+            # create_item do azure.cosmos permite a criação de um item
+            # com os dados que foram repassados no parâmetro
             container.create_item(body)
 
+        # Atualiza um item existente
         elif (option == 4):
             if (members_ids):
                 for idx, member in enumerate(members_ids):
@@ -110,23 +122,28 @@ def showMenu(container, option = 1):
                 readItem['collegeInformation']['course'] = course
                 readItem['collegeInformation']['studentId'] = studentId
     
+                # replace_item do azure.cosmos permite atualização
+                # dos dados de um item
                 container.replace_item(item=readItem, body=readItem)
 
 if __name__ == '__main__':
+
+    # Conexão com CosmosDB
     endpoint = 'https://sistemas-distribuidos-cosmosdb.documents.azure.com:443/'
     key = 'FLrFsEzm7N4eL8ECFpfxFEz5mcSHG6bFYpT47iEyJWbX9WmSTQAe1mSnZiO1Lm40YsX8pkYRSo0Bou91MbHxIg=='
-
     client = CosmosClient(endpoint, key)
+
+    # Instância o database (cria um caso não exista)
     database_name = 'SeminarioSistemasDistribuidos-CosmosDB'
     database = client.create_database_if_not_exists(id=database_name)
 
+    # Instância o container (cria um caso não exista)
     container_name = 'IntegrantesDoGrupo'
     container = database.create_container_if_not_exists(
         id=container_name, 
         partition_key=PartitionKey(path="/id"),
         offer_throughput=400
     )
-    ids_members = returnIDS(container)
     
+    # Inicializa o menu de CRUD
     showMenu(container)
-        
