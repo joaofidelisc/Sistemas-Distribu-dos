9085580b24e7ec9bfa18b92a03b0f65f980144ad
joaofidelisc <joao.fidelis@estudante.ufscar.br>
2022-04-26 00:23:47 -0300

    cosmos_read_and_delete_item atualizado

--- cosmos_read_and_delete_item.py
+++ cosmos_read_and_delete_item.py
@@ -1,8 +1,8 @@
-import azure.cosmos.cosmos_client as cosmos_client
-import azure.cosmos.exceptions as exceptions
-from azure.cosmos.partition_key import PartitionKey
-import datetime
-import cosmos_get_started
+
+from pydoc import doc
+from typing import Any
+from azure.cosmos import CosmosClient, PartitionKey
+# import cosmos_get_started
 # from container import list_Containers
 
 
@@ -10,6 +10,28 @@ HOST = 'https://sistemas-distribuidos-cosmosdb.documents.azure.com:443/'
 MASTER_KEY = 'FLrFsEzm7N4eL8ECFpfxFEz5mcSHG6bFYpT47iEyJWbX9WmSTQAe1mSnZiO1Lm40YsX8pkYRSo0Bou91MbHxIg=='
 DATABASE_ID = 'SeminarioSistemasDistribuidos-CosmosDB'
 CONTAINER_ID = 'IntegrantesDoGrupo'
+
+
+# <create_cosmos_client>
+client = CosmosClient(HOST, MASTER_KEY)
+# </create_cosmos_client>
+
+# Create a database
+# <create_database_if_not_exists>
+database_name = 'SeminarioSistemasDistribuidos-CosmosDB'
+database = client.create_database_if_not_exists(id=database_name)
+# </create_database_if_not_exists>
+
+# Create a container
+# Using a good partition key improves the performance of database operations.
+# <create_container_if_not_exists>
+container_name = 'IntegrantesDoGrupo'
+container = database.create_container_if_not_exists(
+    id=container_name, 
+    partition_key=PartitionKey(path="/lastName"),
+    offer_throughput=400
+)
+# </create_container_if_not_exists>
 
 def read_items(container):
     print('\n1.3 - Reading all items in a container\n')
@@ -29,11 +51,18 @@ def read_item(container, doc_id):
 
     # Note that Reads require a partition key to be spcified.
     response = container.read_item(item=doc_id, partition_key=doc_id)
-
+    # print('doc_id:', doc_id)
+    # print('\n\n')
     print('Item read by Id {0}'.format(doc_id))
     print('lastName: {0}'.format(response.get('lastName')))
-    print('informationsFromSchool: {0}'.format(response.get('informationsFromSchool')))
+    # print('collegeInformation: {0}'.format(response.get('collegeInformation')))
 
-if __name__=='main':
-    # read_items(cosmos_get_started.container)
-    read_item(cosmos_get_started.container, cosmos_get_started.container_name)
+
+def delete_item(container, doc_id):
+    print('\n1.7 Deleting Item by Id\n')
+
+    response = container.delete_item(item=doc_id, partition_key=doc_id)
+
+    print('Deleted item\'s Id is {0}'.format(doc_id))
+
+read_items(container)
